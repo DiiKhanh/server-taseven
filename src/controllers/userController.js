41909fe7e57de4bd343c66b3015ab24f68a1cbed
DiiKhanh <duykhanh.030803@gmail.com>
2024-07-07 13:38:51 +0700

    fix: resolve change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -118,8 +118,6 @@ const getAllUsers = asyncHandle(async (req, res) => {
 
 const getProfile = asyncHandle(async (req, res) => {
   const { id } = req.query
-  const access = await getAccessToken()
-  console.log(access)
 
   if (id) {
     const profile = await UserModel.findOne({ _id: id })
@@ -133,8 +131,7 @@ const getProfile = asyncHandle(async (req, res) => {
         email: profile.email ?? '',
         photoUrl: profile.photoUrl ?? '',
         fullname: profile.fullname ?? '',
-        filename: profile.filename ?? '',
-        access
+        filename: profile.filename ?? ''
       }
     })
   } else {
